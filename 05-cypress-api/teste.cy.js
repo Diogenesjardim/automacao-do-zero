1e01ed8514d27055e395dd5e54338e6277ddee9a
User <davidwallan@gmail.com>
2026-04-20 16:10:38 -0300

    remove Cypress (05-cypress-api e 06-cypress-web), mantém apenas Java

--- 05-cypress-api/teste.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Teste API', () => {
-  it('Retorna 200', () => {
-    cy.request('https://jsonplaceholder.typicode.com/users')
-      .then((res) => {
-        expect(res.status).to.eq(200)
-      })
-  })
-})
