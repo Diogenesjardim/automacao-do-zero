1f24f4b9d7076d045e8baf2b2c196d780dd32d0e
User <davidwallan@gmail.com>
2026-05-07 17:21:34 -0300

    config

--- 04-java-web/src/test/java/com/estudo/steps/testSteps.java
+++ 04-java-web/src/test/java/com/estudo/steps/testSteps.java
@@ -2,14 +2,31 @@ package com.estudo.steps;
 
 import com.estudo.pages.MetodoPage;
 import com.estudo.support.DriverContext;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class testSteps {
+
+    @Before
+    public void beforeScenario() {
+        WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+
+        WebDriver driver = new ChromeDriver(options);
+        DriverContext.setDriver(driver);
+    }
 
     private MetodoPage page;
 
@@ -33,6 +50,15 @@ public class testSteps {
     public void oTituloDaPaginaDeveEstarPreenchido() {
         String titulo = page.obterTitulo();
         assertFalse(titulo == null || titulo.trim().isEmpty(), "O título da página não deveria estar vazio.");
+    }
+    @After
+    public void afterScenario() {
+        try {
+            WebDriver driver = DriverContext.getDriver();
+            driver.quit();
+        } finally {
+            DriverContext.clear();
+        }
     }
 }
 
